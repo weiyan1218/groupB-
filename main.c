b5443afb5810adc402f8eb4ead17ab916ec845e3
陳威諺 <nink931228@gmail.com>
2025-11-25 16:23:27 +0800

    finist sub_float

--- main.c
+++ main.c
@@ -11,12 +11,20 @@ int sub_int(int a,int b)
 }
 
 
+float sub_float(float a,float b)
+{
+    return a-b;
+}
+
 int main()
 {
     int a=5;
     int b=10;
+    float fa=12.3;
+    float fb=5.5;
     printf("%d + %d = %d\n",a,b,add_int(a,b));
     printf("%d - %d = %d\n",a,b,sub_int(a,b));
+    printf("%f - %f = %f\n",fa,fb,sub_float(fa,fb));
     return 0;
 
 }
