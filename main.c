ff958bff00fc1e8ec3a597efb883a13a5ede133c
陳威諺 <nink931228@gmail.com>
2025-11-25 16:02:42 +0800

    the fifth commit

--- main.c
+++ main.c
@@ -5,13 +5,18 @@ int add_int(int a,int b)
     return a+b;
 }
 
+int sub_int(int a,int b)
+{
+    return a-b;
+}
+
 
 int main()
 {
     int a=5;
     int b=10;
     printf("%d + %d = %d\n",a,b,add_int(a,b));
-    printf("great");
+    printf("%d - %d = %d\n",a,b,sub_int(a,b));
     return 0;
 
 }
